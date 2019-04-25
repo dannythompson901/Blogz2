af7e66b565cacd6b69a76ac8084aae009c25d961
Walker Laury <walkerclaury@protonmail.com>
2019-04-25 16:08:08 -0500

    finished part one

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLAlchemy_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:localhost@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:localhost@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-Class Blog(db.Model):
+class Blog(db.Model):
     
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
 
@@ -17,17 +17,31 @@ Class Blog(db.Model):
         self.title = title
         self.body = body
     
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    return render_template('/')
 
 @app.route('/blog')
 def blog():
-    return render_template('/blog')
+    posts = Blog.query.all()
+    return render_template('blog.html', posts = posts)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    return render_template('/blog')
+    title_error = ''
+    text_error = ''
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
 
+        if not title:
+            title_error = 'You need to have a title!'
+        if not body:
+            text_error = 'You need to add content!'
 
-app.run()
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/blog')
+    return render_template('newpost.html')
+if __name__ == '__main__':
+    app.run()
